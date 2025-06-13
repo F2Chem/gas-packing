a2ab5baffd75e5c3aaa89c90277498903924b3c3
ThePix <102726367+F2Chem@users.noreply.github.com>
2025-06-13 13:52:22 +0100

    Changed index to home

--- gas_filling/urls.py
+++ gas_filling/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="index"),
     path("cylinder", views.cylinder_index, name="cylinder_index"),
     path("cylinder/<int:obj_id>", views.cylinder_view, name="cylinder_view"),
     path("cylinder/<int:obj_id>/edit", views.cylinder_edit, name="cylinder_edit"),
